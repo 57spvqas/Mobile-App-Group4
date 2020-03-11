5b8de914cb4df8ccc09b5fe79ba1aea481e7c016
sgibso202 <48760802+sgibso202@users.noreply.github.com>
2020-03-11 20:23:51 +0000

    Login_Register
    - login activity created
    - register activity created

--- cw2/app/src/main/java/com/example/mobdev_cw2/LoginPage.java
+++ cw2/app/src/main/java/com/example/mobdev_cw2/LoginPage.java
@@ -2,13 +2,19 @@ package com.example.mobdev_cw2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_page);
+
+        Intent intent = getIntent();
     }
+
+
 }
