39aa1dcc82544dfb729d90a79ae0446b8280ceae
sgibso202 <48760802+sgibso202@users.noreply.github.com>
2020-03-29 14:09:15 +0100

    Added Dark Mode to Settings
    - Dark Mode functionality has been added to the Settings page only

--- MentalBrainApplication1.9/app/src/main/java/com/example/mentalbrainapplicaiton/MainActivity.java
+++ MentalBrainApplication1.9/app/src/main/java/com/example/mentalbrainapplicaiton/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 
 /**
@@ -12,10 +13,13 @@ import android.view.View;
  */
 public class MainActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     /**
--- MentalBrainApplication1.9/app/src/main/java/com/example/mentalbrainapplicaiton/Settings.java
+++ MentalBrainApplication1.9/app/src/main/java/com/example/mentalbrainapplicaiton/Settings.java
@@ -1,21 +1,63 @@
 package com.example.mentalbrainapplicaiton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Settings extends AppCompatActivity {
 
+    static private String darkMode = "false";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        final Switch newSwitch = (Switch) findViewById(R.id.switch1);
+        final TextView tv = findViewById(R.id.textView36);
+        final View root = tv.getRootView();
+        //root.setBackgroundColor(Color.parseColor("#000000"));
+        root.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.gradient));
+
+        newSwitch.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View view) {
+
+                if(newSwitch.isChecked()){
+                    darkMode = "true";
+                    root.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.background));
+
+                }
+                else {
+                    darkMode = "false";
+                   root.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.gradient));
+
+                }
+
+            }
+        });
     }
+
+    /***
+     * Method to return current setting of "dark mode" switch
+     */
+    static public String getDarkMode()
+    {
+        return darkMode;
+    }
+
 
     /***
      * Method to be used to return to the main menu.  Called the user selects the 'Home' button.
